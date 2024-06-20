3993bc00143cafd5826a54c904511fe5f3e02369
The_Situation <40388793+lasituation@users.noreply.github.com>
2024-06-20 17:27:12 +0100

    fix

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post("/sponsor", (req, res) => {
   const { name, surname, email, number, company } = req.body;
 
   if (!email) {
-    return res.status(400).json("Entrer une adresse mail");
+    return res.status(400).json("Entrez une adresse mail");
   }
 
   const mailOptions = {
